73e34932e90980da14ce091733d3967753eb0685
Wonderful Carrot <wonderarry@gmail.com>
2023-10-02 00:28:05 +0300

    moved bash script and added to dockerfile

--- client/prodstart.sh
+++ client/prodstart.sh
@@ -2,10 +2,13 @@
 
 # Step 1: Test and reload Nginx configuration
 nginx -t
-nginx -g 'daemon off;'
+nginx
 
 # Step 2: Request and renew the SSL certificate using Certbot
 certbot --nginx -d tournaments.megahello.ee --email wonderarry@gmail.com --agree-tos -v
 
 # Step 3: Add a cron job to renew the certificate daily at 12:00 PM
 (crontab -l ; echo "0 12 * * * /usr/bin/certbot renew --quiet") | crontab -
+
+# Step 4: Run in foreground
+nginx -g 'daemon off;'
