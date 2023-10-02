dcdd1c962eeac68164445637c1eae2dc06a8918b
Wonderful Carrot <wonderarry@gmail.com>
2023-10-02 03:19:34 +0300

    finally working certbot

--- client/prodstart.sh
+++ client/prodstart.sh
@@ -7,5 +7,5 @@ nginx
 # Step 2: Request and renew the SSL certificate using Certbot
 certbot --nginx -d tournaments.megahello.ee --email wonderarry@gmail.com --agree-tos -v
 
-# Step 3: Add a cron job to renew the certificate daily at 12:00 PM
-# (crontab -l ; echo "0 12 * * * /usr/bin/certbot renew --quiet") | crontab -
+# Step 3: Make it run permanently
+tail -f /dev/null
