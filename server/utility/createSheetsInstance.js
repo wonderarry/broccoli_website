97b6d1ef7652956964966ad0804087f8cc1930bb
Wonderful Carrot <wonderarry@gmail.com>
2023-10-03 03:21:53 +0300

    tried to fix .env processing as raw file in private key

--- server/utility/createSheetsInstance.js
+++ server/utility/createSheetsInstance.js
@@ -4,7 +4,8 @@ export async function createSheetsInstance(){
     const auth = new google.auth.GoogleAuth({
         credentials: {
             client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-            private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
+            private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n")
+            ,
         },
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
