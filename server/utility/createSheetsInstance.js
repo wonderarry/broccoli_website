ba88c90a0f77c3c1531dad8128238e4e90b0cca3
Wonderful Carrot <wonderarry@gmail.com>
2023-10-03 03:37:30 +0300

    temporarily logging the process env file

--- server/utility/createSheetsInstance.js
+++ server/utility/createSheetsInstance.js
@@ -4,8 +4,7 @@ export async function createSheetsInstance(){
     const auth = new google.auth.GoogleAuth({
         credentials: {
             client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-            private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n")
-            ,
+            private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
         },
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
